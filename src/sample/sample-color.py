6b1b9d6b7be097f5b3b56e3e92dd50fe4d11e8ce
ituki <ituki@usernoMacBook-Pro.local>
2023-08-13 15:14:12 +0900

    顔検出の実装

--- src/sample/sample-color.py
+++ src/sample/sample-color.py
@@ -3,8 +3,15 @@ import cv2
 
 def main():
     cap = cv2.VideoCapture(0)
+    cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
     while(True):
-        ret, frame = cap.read()
+        _, frame = cap.read()
+        fps = cap.get(cv2.CAP_PROP_FPS)
+        cv2.putText(frame, "{}".format(fps), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), thickness=2)
+        lists = cascade.detectMultiScale(frame, minSize=(100, 100))
+        if len(lists):
+            for (x,y,w,h) in lists:
+                cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 0, 255), thickness=2)
         cv2.imshow("frame", frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
